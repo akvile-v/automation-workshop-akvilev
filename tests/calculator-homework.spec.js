2a0c6a34f0039d83d5930c67f0256b3971a58b11
akvile-v <akvile.visniauskaite@gmail.com>
2021-08-11 12:25:42 +0300

    Randomised input numbers, more formated

--- tests/calculator-homework.spec.js
+++ tests/calculator-homework.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 const { HomeworkStartPage } = require('../homework-pages/homework-start-page');
 
-const buildSelector= ['0', '1', '2', '3', '4','5', '6', '7', '8', '9'];
+const buildSelector = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 buildSelector.forEach(calculatorBuild => {
   test.describe('', () => {
     let page;
@@ -13,58 +13,66 @@ buildSelector.forEach(calculatorBuild => {
       await startPage.goto(calculatorBuild);
     });
 
-  test.only(`Testing if calculator build ${calculatorBuild} is loading with all fields`, async () => {
-    const formFields = ['#calcForm', '#number1Field', '#number2Field', '#selectOperationDropdown', 
-                        '#calculateButton', '#numberAnswerField', '#integerSelect', '#clearButton'];
-   for (let i = 0; i < formFields.length; i++) {
-      const calculatorForm = await page.isVisible(formFields[i]);
-      expect(calculatorForm).toBe(true);
-    }
-  });
-
-  test.only(`Testing calculator buil ${calculatorBuild} simple calculations with positive and negative numbers `, async () => {
-    const dropdownObjects = ['0', '1', '2', '3', '4'];
-    for (let i = 0; i < 5; i++) {
-      await startPage.simpleCalculation('10', '3', dropdownObjects[i]);
-      let expectedValue = 10 + 3;
-      const answerFieldResults = await page.inputValue('#numberAnswerField');
-      switch(dropdownObjects[i]) {
-        case '1':
-          expectedValue = 10 - 3;
-          break;
-        case '2':
-          expectedValue = 10 * 3;
-          break;
-        case '3':
-          expectedValue = 10 / 3;
-          break;
-        case '4':
-          expectedValue = '103';
+    test.only(`Testing if calculator build ${calculatorBuild} is loading with all fields`, async () => {
+      const formFields = ['#calcForm', '#number1Field', '#number2Field', '#selectOperationDropdown',
+        '#calculateButton', '#numberAnswerField', '#integerSelect', '#clearButton'];
+      for (let i = 0; i < formFields.length; i++) {
+        const calculatorForm = await page.isVisible(formFields[i]);
+        expect(calculatorForm).toBe(true);
       }
-      expect(answerFieldResults).toBe("" + expectedValue + "");
-    }
-  }); 
+    });
 
-  test.only(`Testing calculator build ${calculatorBuild} division by 0`, async () => {
-    await startPage.simpleCalculation('5', '0', '3');
-    const errorMessage = await page.isVisible('#errorMsgField');
-    expect(errorMessage).toBe(true);
-  }); 
+    test.only(`Testing calculator build ${calculatorBuild} simple calculations`, async () => {
+      const dropdownObjects = ['0', '1', '2', '3', '4'];
+      let num1 = Math.floor(Math.random() * 50) + 1;
+      let num2 = Math.floor(Math.random() * 50) + 1;
+      for (let i = 0; i < 5; i++) {
+        await startPage.simpleCalculation(num1.toString(), num2.toString(), dropdownObjects[i]);
+        let expectedValue = num1 + num2;
+        const answerFieldResults = await page.inputValue('#numberAnswerField');
+        switch (dropdownObjects[i]) {
+          case '1':
+            expectedValue = num1 - num2;
+            break;
+          case '2':
+            expectedValue = num1 * num2;
+            break;
+          case '3':
+            expectedValue = num1 / num2;
+            break;
+          case '4':
+            expectedValue = num1.toString() + num2.toString();
+        }
+        expect(answerFieldResults).toBe(expectedValue.toString());
+      }
+    });
 
-  test.only(`Testing if integer checkmark works in calculator build ${calculatorBuild}`, async () => {
-    await startPage.simpleCalculation('10', '3', '3');
-    await page.click('#integerSelect');
-    const AnswerFieldResults = await page.inputValue('#numberAnswerField');
-    const ExpectedValue = Math.trunc(10/3);
-    expect(AnswerFieldResults).toBe("" + ExpectedValue + ""); 
-  });
-          
-  test.only(`Testing if clear button works in calculator build ${calculatorBuild}`, async () => {
-    await startPage.simpleCalculation('10', '3', '0');
-    await page.click('#clearButton');
-    const AnswerFieldResults = await page.inputValue('#numberAnswerField');
-    expect(AnswerFieldResults).toBe("");
-  });
+    test.only(`Testing calculator build ${calculatorBuild} division by 0`, async () => {
+      let num = Math.floor(Math.random() * 50) + 1;
+      await startPage.simpleCalculation(num.toString(), '0', '3');
+      const errorMessage = await page.isVisible('#errorMsgField');
+      expect(errorMessage).toBe(true);
+    });
+
+    test.only(`Testing if integer checkmark works in calculator build ${calculatorBuild}`, async () => {
+      const isCheckmarkWorking = await page.isEnabled("#integerSelect");
+      expect(isCheckmarkWorking).toBe(true);
+      if (!isCheckmarkWorking) { return; }
+      await startPage.simpleCalculation('10', '3', '3');
+      await page.click('#integerSelect');
+      const answerFieldResults = await page.inputValue('#numberAnswerField');
+      const expectedValue = Math.trunc(10 / 3);
+      expect(answerFieldResults).toBe(expectedValue.toString());
+    });
+
+    test.only(`Testing if clear button works in calculator build ${calculatorBuild}`, async () => {
+      let num1 = Math.floor(Math.random() * 50) + 1;
+      let num2 = Math.floor(Math.random() * 50) + 1;
+      await startPage.simpleCalculation(num1.toString(), num2.toString(), '0');
+      await page.click('#clearButton');
+      const answerFieldResults = await page.inputValue('#numberAnswerField');
+      expect(answerFieldResults).toBe("");
+    });
 
   });
 });
@@ -85,7 +93,7 @@ buildSelector.forEach(calculatorBuild => {
             else if (DropdownObjects[i] == '4'){  ExpectedValue = '-100-123';}
       expect(AnswerFieldResults).toBe("" + ExpectedValue + "");
     }
-  }); 
+  });
 test('Test for testing', async ({page}) => {
     await page.goto('https://testsheepnz.github.io/BasicCalculator')
     //await page.click('#selectBuild');
@@ -97,4 +105,4 @@ test('Test for testing', async ({page}) => {
       const AnswerFieldResults = await page.inputValue('#numberAnswerField');
       const ExpectedValue = 5-7;
       expect(AnswerFieldResults).toBe("" + ExpectedValue + "");
-    }); */ 
+    }); */
